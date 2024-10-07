3e14010ac814b5d44e2da217cbcf85888fefd2a6
astridyusara <138843351+astridyusara@users.noreply.github.com>
2024-10-07 17:34:09 +0900

    Delete src/MATCRO/MATCRO_compile.sh

--- src/MATCRO/MATCRO_compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-ifort -CB -fpe0 -traceback -g -o MATCRO-Soy MATCRO.f90 SUB_IO.f90 SUB_TINTERP.f90 SUB_RAD.f90 SUB_PHSYN.f90 SUB_SOIL.f90 SUB_CROP.f90 FNC.f90 -I/home/soft/local/netcdf/include -L/home/soft/local/netcdf/lib -lnetcdff
-
-
-
